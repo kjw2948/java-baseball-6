b55833eba422aece28d1f50e3c2cc5e77c416212
kjw2948 <kjw970313@cau.ac.kr>
2023-10-22 23:22:49 +0900

    feat : 나의 숫자입력 기능 추가

--- src/main/java/baseball/InputMyNum.java
+++ src/main/java/baseball/InputMyNum.java
@@ -1,2 +1,16 @@
-package baseball;public class InputMyNum {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+public class InputMyNum {
+    public List<Integer> inputNum(){
+        String playerNumString = Console.readLine();
+        List<Integer> playerNum = new ArrayList<>();
+        for(String number : playerNumString.split("")){
+            playerNum.add(Integer.parseInt(number));
+        }
+        return playerNum;
+    }
 }
--- src/main/java/baseball/MakeNumBaseball.java
+++ src/main/java/baseball/MakeNumBaseball.java
@@ -3,7 +3,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class MakeNumBaseball {
     public List<Integer> makeRandom() {
